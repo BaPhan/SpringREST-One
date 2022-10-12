79f277cf2fb719cfbb6a13cb8943a33f3b2c0cc1
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:37:14 +0700

    Update CommentRepository.java

--- src/main/java/com/springrestone/repository/CommentRepository.java
+++ src/main/java/com/springrestone/repository/CommentRepository.java
@@ -1,2 +1,7 @@
-package com.springrestone.repository;public interface CommentRepository {
+package com.springrestone.repository;
+
+import com.springrestone.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
