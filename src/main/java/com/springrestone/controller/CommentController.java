7716dec6548a5e930bb986e20d29060cb016c472
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:33:26 +0700

    Update CommentController.java

--- src/main/java/com/springrestone/controller/CommentController.java
+++ src/main/java/com/springrestone/controller/CommentController.java
@@ -1,2 +1,43 @@
-package com.springrestone.controller;public class CommentController {
+package com.springrestone.controller;
+import com.springrestone.service.CommentService;
+import com.springrestone.ultil.request.CommentDTO;
+import com.springrestone.ultil.response.CommentResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/comment")
+@CrossOrigin("*")
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+    @GetMapping("/findAll")
+    public ResponseEntity<List<CommentResponseDTO>> findAll(){
+        return new ResponseEntity<>(commentService.getAll(), HttpStatus.OK);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<CommentResponseDTO> create(@RequestBody CommentDTO dto){
+        return new ResponseEntity<>(commentService.save(dto),HttpStatus.CREATED);
+    }
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<CommentResponseDTO> findById(@PathVariable Long id){
+        Optional<CommentResponseDTO> commentResponseDTO = commentService.findById(id);
+        return new ResponseEntity<>(commentResponseDTO.get(),HttpStatus.OK);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CommentResponseDTO> update(@RequestBody CommentDTO dto,@PathVariable Long id){
+        Optional<CommentResponseDTO> commentResponseDTO = commentService.findById(id);
+        return new ResponseEntity<>(commentService.update(dto,id),HttpStatus.OK);
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<CommentResponseDTO> delete(@PathVariable Long id){
+        Optional<CommentResponseDTO> commentResponseDTO = commentService.findById(id);
+        commentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
