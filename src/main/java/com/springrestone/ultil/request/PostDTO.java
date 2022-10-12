d814254e3c7741065eebc06647f8e02f0eafc321
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:41:15 +0700

    Update PostDTO.java

--- src/main/java/com/springrestone/ultil/request/PostDTO.java
+++ src/main/java/com/springrestone/ultil/request/PostDTO.java
@@ -1,19 +1,23 @@
-package com.springrestone.dto;
+package com.springrestone.ultil.request;
 
-import com.springrestone.entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDTO {
+    @NotNull(message = "title is not requrire")
+    @NotEmpty(message = "field is not empty")
     private String title;
+    @NotNull(message = "description is not required")
+    @NotEmpty(message = "description is not empty")
     private String description;
     private String content;
-    private Set<CommentDTO> comments;
+//    private Set<CommentDTO> comments;
 }
