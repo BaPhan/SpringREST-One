afb026270b2f640670173eb360423f4409f712b2
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:41:42 +0700

    Update CommentResponseDTO.java

--- src/main/java/com/springrestone/ultil/response/CommentResponseDTO.java
+++ src/main/java/com/springrestone/ultil/response/CommentResponseDTO.java
@@ -1,2 +1,21 @@
-package com.springrestone.ultil.response;public class CommentResponseDTO {
+package com.springrestone.ultil.response;
+
+import com.springrestone.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
+    private String posttitle;
 }
