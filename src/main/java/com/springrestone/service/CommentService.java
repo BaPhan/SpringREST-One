e4bc6fb45d2fa7cdbc11671e384cee90870cb339
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:38:11 +0700

    Update CommentService.java

--- src/main/java/com/springrestone/service/CommentService.java
+++ src/main/java/com/springrestone/service/CommentService.java
@@ -1,2 +1,18 @@
-package com.springrestone.service;public interface CommentService {
+package com.springrestone.service;
+
+import com.springrestone.entity.Comment;
+import com.springrestone.ultil.request.CommentDTO;
+import com.springrestone.ultil.request.PostDTO;
+import com.springrestone.ultil.response.CommentResponseDTO;
+import com.springrestone.ultil.response.PostResponseDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentService {
+    public List<CommentResponseDTO> getAll();
+    public Optional<CommentResponseDTO> findById(Long id);
+    public CommentResponseDTO save(CommentDTO dto);
+    public CommentResponseDTO update(CommentDTO dto,Long id);
+    public String delete(Long id);;
 }
