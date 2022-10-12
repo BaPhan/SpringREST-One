d08670e2e3cffc434fdcd4e5bd6dc0809bd3b4e0
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:40:56 +0700

    Update CommentDTO.java

--- src/main/java/com/springrestone/ultil/request/CommentDTO.java
+++ src/main/java/com/springrestone/ultil/request/CommentDTO.java
@@ -1,11 +1,9 @@
-package com.springrestone.dto;
+package com.springrestone.ultil.request;
 
 import com.springrestone.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -14,6 +12,7 @@ public class CommentDTO {
     private String name;
     private String email;
     private String body;
-    private Post post;
+    //private Post post;
+    private Long postId;
 }
 
