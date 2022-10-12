b539b66d9e15f66917e15c11a94584131fc54de2
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:38:34 +0700

    Update PostService.java

--- src/main/java/com/springrestone/service/PostService.java
+++ src/main/java/com/springrestone/service/PostService.java
@@ -1,2 +1,16 @@
-package com.springrestone.service;public interface PostService {
+package com.springrestone.service;
+
+import com.springrestone.ultil.request.PostDTO;
+import com.springrestone.ultil.response.PostResponseDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostService {
+    public List<PostResponseDTO> getAll();
+    public Optional<PostResponseDTO> findById(Long id);
+    public PostResponseDTO save(PostDTO dto);
+    public PostResponseDTO update(PostDTO dto,Long id);
+    public String delete(Long id);
+
 }
