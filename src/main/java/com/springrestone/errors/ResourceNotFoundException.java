177db817973c81b2f5c3ca6d37476ddeec934228
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:36:19 +0700

    Update ResourceNotFoundException.java

--- src/main/java/com/springrestone/errors/ResourceNotFoundException.java
+++ src/main/java/com/springrestone/errors/ResourceNotFoundException.java
@@ -1,2 +1,16 @@
-package com.springrestone.errors;public class ResourceNotFoundException {
+package com.springrestone.errors;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private String message;
+
+    public ResourceNotFoundException (String message){
+        this.message = message;
+    }
+
 }
