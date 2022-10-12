5098aa2b1f8533391cd2749e6f46ba40458ec0a9
BaPhan <70484430+BaPhan@users.noreply.github.com>
2022-10-13 00:42:04 +0700

    Update PostResponseDTO.java

--- src/main/java/com/springrestone/ultil/response/PostResponseDTO.java
+++ src/main/java/com/springrestone/ultil/response/PostResponseDTO.java
@@ -1,2 +1,26 @@
-package com.springrestone.ultil.response;public class PostResponseDTO {
+package com.springrestone.ultil.response;
+
+import com.springrestone.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+    private Integer maximumOfContent;
+    private Set<CommentResponseDTO> comments;
+
 }
